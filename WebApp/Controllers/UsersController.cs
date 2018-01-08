f92eecfab33cb8bc177715fd4a872f6d256d6133
sapirbh <sapirbh98@gmail.com>
2018-01-08 10:34:40 +0200

    First commit

---
+++ WebApp/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string UserEmail, string Password)
+        {
+            UsersDataAccess u = new UsersDataAccess();
+
+            var requestedUser = u.CheckLogin(UserEmail, Password);
+            if(requestedUser == null)
+            {
+                return Json(new
+                {
+                    Success = false
+                });
+            }
+            else
+            {
+                System.Web.HttpContext.Current.Session["user"] = requestedUser.Id;
+                System.Web.HttpContext.Current.Session.Timeout = 30;
+                return View();
+            }
+        }
+
+        public ActionResult AddNewUser(string UserEmail, string FirstName, string LastName, string Password)
+        {
+            UsersDataAccess u = new UsersDataAccess();
+            var newUser = u.AddUser(UserEmail, FirstName, LastName, Password);
+            if (newUser == null)
+            {
+                return Json(new
+                {
+                    Success = false
+                });
+            }
+            else
+            {
+                System.Web.HttpContext.Current.Session["user"] = newUser.Id;
+                System.Web.HttpContext.Current.Session.Timeout = 30;
+
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ WebApp/Models/UsersDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class UsersDataAccess
+    {
+        ypaEntities db;
+
+        public user CheckLogin(string userEmail, string password)
+        {
+            return db.users.Where(u => u.Email == userEmail && u.Password == password).FirstOrDefault();
+        }
+
+        public user AddUser(string userEmail, string firstName, string lastName, string password)
+        {
+            if (db.users.Any(u => u.Email == userEmail))
+                return null;
+            user NewUser = new user(firstName, lastName, userEmail, password);
+            db.users.Add(NewUser);
+            return NewUser;
+        }
+
+    }
+}
